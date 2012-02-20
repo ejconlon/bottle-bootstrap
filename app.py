c0ec1dbcaaabf90372f59ed178317b21c8777d07
Eric Conlon <ejconlon@gmail.com>
2012-02-19 21:26:53 -0500

    adding bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
 import os
-from bottle import route, run
+from bottle import route, run, static_file, template, view
+
+@route('/js/<filename>')
+def js_static(filename):
+    return static_file(filename, root='./js')
+
+@route('/img/<filename>')
+def img_static(filename):
+    return static_file(filename, root='./img')
+
+@route('/css/<filename>')
+def img_static(filename):
+    return static_file(filename, root='./css')
 
 @route("/")
+@view("main")
 def hello():
-    return "Hello from Python!"
+    return dict(title = "Hello", content = "Hello from Python!")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
